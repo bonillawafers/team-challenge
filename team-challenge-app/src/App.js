9e44b3c4a21666c77d98083c4c8a774ed8f1a255
Marissa Exelby <mexelby@uw.edu>
2016-11-28 01:42:42 -0800

    finished adding project structure

--- team-challenge-app/src/App.js
+++ team-challenge-app/src/App.js
@@ -1,20 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 import { Navbar, Jumbotron, Button } from 'react-bootstrap';
-import { SignUpForm } from './TeamSignup.js';
+import SignUpForm from './TeamSignUp.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React *Change</h2>
+
+          <h2>Sign Up!</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Our service is hella lit but you must be at least 13 years old to join
         </p>
+        <SignUpForm />
       </div>
     );
   }
